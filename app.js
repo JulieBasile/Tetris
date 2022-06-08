32b96a014abc0fe6a7cd96b73283e7f8fac91507
Julie Basile <hello@jebasile.com>
2022-06-07 21:11:24 -0400

    basic grid + squares created

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //all code encapsulates here
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10 
+    
+
+
+})
