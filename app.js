3d1d8f0e3c5505f13d454274c80aea63f4f59f16
Julie Basile <hello@jebasile.com>
2022-06-09 20:19:37 -0400

    tetris shapes (tetrominoes) added

--- app.js
+++ app.js
@@ -6,6 +6,65 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10 
     
+    /*Tetrominoes (4 tetris shapes)*/ 
+    const lTetromino = [
+        [1,width+1,width*2+1,2],
+        [width,width+1,width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2],
+    ]
 
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+    ] 
 
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1],
+    ] 
+
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+    ] 
+
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+    ] 
 })
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+let currentPosition = 5
+let currentRotation = 0
+
+console.log(theTetrominoes[0][0])
+
+//randomly select tetromino and its first rotation
+let random = Math.floor(Math.random()*theTetrominoes.length)
+let current = theTetrominoes[random] [currentRotation]
+
+//drawing the tetromino
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+        squares[currentPosition + index].style.backgroundColor = color[random]
+    })
+}
+
+//undrawing the tetromino
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+        squares[currentPosition + index].style.backgroundColor = ''
+    })
+}
